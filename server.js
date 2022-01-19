3dd2f3b1c7e8a07b668ed825c4666b3ce4999781
MichValenz <michvalenz27@gmail.com>
2022-01-18 22:49:30 -0600

    added queries

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require("mysql2");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "gatosgatas31!",
+    database: "empTracker",
+  },
+  console.log("Connected to the election database.")
+);
 
 
 
-
-/// the GET and POST section/////
+/// the query, GET and POST section/////
 app.get("/", (req, res) => {
   res.json({
     message: "Hello World",
@@ -17,7 +26,17 @@ app.get("/", (req, res) => {
 });
 
 
+db.query(`SELECT * FROM department`, (err, rows) => {
+  console.log(rows);
+});
 
+db.query(`SELECT * FROM role`, (err, rows) => {
+  console.log(rows);
+});
+
+db.query(`SELECT * FROM employee`, (err, rows) => {
+  console.log(rows);
+});
 
 
 app.use((req, res) => {
